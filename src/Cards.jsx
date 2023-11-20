9bac5f5d72b7ad8fd5795b66bcdd721fa8a4c2ad
matthew-rayman <maungyemamn@gmail.com>
2023-11-20 23:47:49 +0630

    mobile edit

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Cards = ({name,image,id}) => {
     
   return (
-    <div className='relative parent w-[250px] justify-center h-[260px] mb-8 '>
+    <div className='relative parent h-[105px] w-[130px] md:w-[250px] justify-center md:h-[220px] mb-8 '>
       <Link to={`/detail/${id}`}>
-        <img className='image h-[250px] w-[230px] object-cover rounded-lg' src={image} alt="" />
-        <p className=' icon bg-orange flex align-middle items-center justify-center h-12 w-12 rounded-[100%] absolute top-[40%] left-[37%]'>
+        <img className='image h-[100px] w-[130px] md:h-[210px] md:w-[230px] object-cover rounded-lg' src={image} alt="" />
+        <p className=' icon bg-orange flex align-middle items-center justify-center h-9 w-9 md:h-12 md:w-12 rounded-[100%] absolute top-[30%] md:top-[40%] left-[37%]'>
           <BiSearch className='text-3xl text-white'/>
         </p>
-        <p className="text-brown font-medium mt-1 text-center">{name.substring(0,20)}{name.length>20? "..." :""}</p>
+        <p className="text-brown opacity-90 text-xs md:text-base md:font-medium mt-1 text-center">{name.substring(0,20)}{name.length>20? "..." :""}</p>
       </Link>
     </div>
   )
